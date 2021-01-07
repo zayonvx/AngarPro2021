5ee02cd36d4d725d374b1582fb7dcd6dad9efd1b
romez <svetbox@gmail.com>
2021-01-07 05:43:20 +0300

    next one

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
-import Head from "next/head";
-import React from "react";
-import { Provider } from "react-redux";
-import store from "../src/store/store";
+import Head from 'next/head';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from '../src/store/store';
+import { SvgSprite } from '../src/js/components/svg-sprite/svg-sprite';
+import { Main } from '../src/js/components/main/main';
 
 export default function Home() {
   return (
@@ -13,42 +15,23 @@ export default function Home() {
           name="Description"
           content="Сайт компании АнгарПро. Проектирование, производство и строительство быстровозводимых зданий."
         />
-        <link
-          rel="icon"
-          href="https://angarpro.com/favicon.svg"
-          type="image/svg+xml"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="https://angarpro.com/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="https://angarpro.com/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="https://angarpro.com/favicon-16x16.png"
-        />
-        <link rel="manifest" href={"/site.webmanifest"} />
-        <link
-          rel="mask-icon"
-          href="https://angarpro.com/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <title>{"АнгарПро - строительство быстровозводимых зданий"}</title>
-        <link
-          rel="stylesheet"
-          href={"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"}
-        />
+        <link rel="icon" href="https://angarpro.com/favicon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" sizes="180x180" href="https://angarpro.com/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="https://angarpro.com/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="https://angarpro.com/favicon-16x16.png" />
+        <link rel="manifest" href={'/site.webmanifest'} />
+        <link rel="mask-icon" href="https://angarpro.com/safari-pinned-tab.svg" color="#5bbad5" />
+        <title>{'АнгарПро - строительство быстровозводимых зданий'}</title>
+        <link rel="stylesheet" href={'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css'} />
         <meta name="msapplication-TileColor" content="#2b5797" />
         <meta name="theme-color" content="#ffffff" />
       </Head>
+
+      <div className="visHidden">
+        <SvgSprite />
+      </div>
+
+      <Main />
     </Provider>
   );
 }
---
+++ src/js/components/main/main.jsx
@@ -0,0 +1,9 @@
+import styles from './main.module.scss'
+import React from "react";
+
+export const Main = () => {
+    return(
+    <div className={styles.wrapper}>
+        Main Page
+    </div>)
+}
