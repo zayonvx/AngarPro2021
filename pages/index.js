0de92c7c300e67d06cd2518626adf64b9f2532e9
romez <svetbox@gmail.com>
2021-01-07 04:29:25 +0300

    feat: initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,54 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import React from "react";
+import { Provider } from "react-redux";
+import store from "../src/store/store";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <Provider store={store}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="canonical" href="https://angarpro.com/" />
+        <meta
+          name="Description"
+          content="Сайт компании АнгарПро. Проектирование, производство и строительство быстровозводимых зданий."
+        />
+        <link
+          rel="icon"
+          href="https://angarpro.com/favicon.svg"
+          type="image/svg+xml"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://angarpro.com/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://angarpro.com/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://angarpro.com/favicon-16x16.png"
+        />
+        <link rel="manifest" href={"/site.webmanifest"} />
+        <link
+          rel="mask-icon"
+          href="https://angarpro.com/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <title>{"АнгарПро - строительство быстровозводимых зданий"}</title>
+        <link
+          rel="stylesheet"
+          href={"https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"}
+        />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    </Provider>
+  );
 }
