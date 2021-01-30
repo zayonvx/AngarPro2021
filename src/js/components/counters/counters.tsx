52ab560dcd0103f14c5515af74154db74bf3e81a
romez <svetbox@gmail.com>
2021-01-30 15:15:28 +0300

    refactor odometers

--- src/js/components/counters/counters.tsx
+++ src/js/components/counters/counters.tsx
@@ -1,39 +1,21 @@
 import React from 'react';
 import styles from './counters.module.scss';
+import odometers from '../../database/odometers';
 
 const Counters = (): JSX.Element => (
   <section className={styles.wrapper}>
     <div className={styles.bg_overlay} />
-    <div className={styles.row}>
-      <div className={styles.cell}>
-        <div className={styles.icon}>
-          <span className="fal fa-hourglass-half" />
-        </div>
-        <span className={styles.digit}>14</span>
-        <span className={styles.text}>Лет на рынке</span>
-      </div>
-      <div className={styles.cell}>
-        <div className={styles.icon}>
-          <span className="fal fa-industry" />
-        </div>
-        <span className={styles.digit}>3</span>
-        <span className={styles.text}>Фабрики</span>
-      </div>
-      <div className={styles.cell}>
-        <div className={styles.icon}>
-          <span className="fal fa-user-friends" />
-        </div>
-        <span className={styles.digit}>23</span>
-        <span className={styles.text}>Сотрудника</span>
-      </div>
-      <div className={styles.cell}>
-        <div className={styles.icon}>
-          <span className="fal fa-home-lg-alt" />
-        </div>
-        <span className={styles.digit}>40</span>
-        <span className={styles.text}>Объектов сдано</span>
-      </div>
-    </div>
+    <ul className={styles.list}>
+      {odometers.map((it) => (
+        <li className={styles.listItem} key={it.id}>
+          <div className={styles.icon}>
+            <span className={it.icon} />
+          </div>
+          <span className={styles.digit}>{it.digit}</span>
+          <span className={styles.text}>{it.text}</span>
+        </li>
+      ))}
+    </ul>
   </section>
 );
 
---
+++ src/js/database/odometers.ts
@@ -0,0 +1,28 @@
+const odometers = [
+  {
+    id: 1,
+    icon: 'fal fa-hourglass-half',
+    digit: '14',
+    text: 'Лет на рынке',
+  },
+  {
+    id: 2,
+    icon: 'fal fa-industry',
+    digit: '3',
+    text: 'Фабрики',
+  },
+  {
+    id: 3,
+    icon: 'fal fa-home-lg-alt',
+    digit: '40',
+    text: 'Cдано объектов',
+  },
+  {
+    id: 4,
+    icon: 'fal fa-th',
+    digit: '300,000',
+    text: 'Квадратных метров',
+  },
+];
+
+export default odometers;
