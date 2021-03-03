6d5b33f1644d2c254b555e3c0235cb7e3c6b92af
Sveta Zayonchkovskaya <svetataburetkina@gmail.com>
2021-03-03 21:50:54 +0300

    add odometer

--- src/js/components/odometers/odometers.tsx
+++ src/js/components/odometers/odometers.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './odometers.module.scss';
 import odometers from '../../database/odometers';
-// TODO create animations of odometers
-// TODO change 300000 meters to objects count
-const Odometers = (): JSX.Element => (
-  <section className={styles.wrapper}>
-    <div className={styles.bg_overlay} />
-    <ul className={styles.list}>
-      {odometers.map((it) => (
-        <li className={styles.listItem} key={it.id}>
-          <div className={styles.icon}>
-            <span className={it.icon} />
-          </div>
-          <span className={styles.digit}>{it.digit}</span>
-          <span className={styles.text}>{it.text}</span>
-        </li>
-      ))}
-    </ul>
-  </section>
-);
+
+const isOnVisibleSpace = (element: Element): boolean => {
+  const bodyHeight = window.innerHeight;
+  const elemRect = element.getBoundingClientRect();
+  const offset = elemRect.top; // - bodyRect.top;
+  if (offset < 0) return false;
+  return offset <= bodyHeight;
+};
+
+const Odometers = (): JSX.Element => {
+  useEffect(() => {
+    const odometerElements = document.querySelectorAll('.odometer') as NodeList;
+
+    window.addEventListener('scroll', () => {
+      odometerElements.forEach((it) => {
+        const element = it as Element;
+        const visible = isOnVisibleSpace(element);
+        if (visible) {
+          element.innerHTML = element.id;
+        }
+      });
+    });
+  });
+  return (
+    <section className={styles.wrapper}>
+      <div className={styles.bg_overlay} />
+      <ul className={styles.list}>
+        {odometers.map((it) => (
+          <li className={styles.listItem} key={it.id}>
+            <div className={styles.icon}>
+              <span className={it.icon} />
+            </div>
+            <span className={`${styles.digit} odometer`} id={it.digit}>
+              {0}
+            </span>
+            <span className={styles.text}>{it.text}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
 
 export default Odometers;
--- src/js/database/odometers.ts
+++ src/js/database/odometers.ts
@@ -1,27 +1,27 @@
 const odometers = [
   {
-    id: 1,
+    id: 'years',
     icon: 'fal fa-hourglass-half',
     digit: '14',
     text: 'Лет на рынке',
   },
   {
-    id: 2,
+    id: 'factories',
     icon: 'fal fa-industry',
     digit: '3',
     text: 'Фабрики',
   },
   {
-    id: 3,
+    id: 'objects',
     icon: 'fal fa-home-lg-alt',
     digit: '40',
     text: 'Cдано объектов',
   },
   {
-    id: 4,
-    icon: 'fal fa-th',
-    digit: '300,000',
-    text: 'Квадратных метров',
+    id: 'employees',
+    icon: 'fal fa-users',
+    digit: '25',
+    text: 'Сотрудников',
   },
 ];
 
