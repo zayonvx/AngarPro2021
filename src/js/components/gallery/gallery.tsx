3cd7b5701b73dc236a7a01265443aaf4a7d4ef2b
romez <svetbox@gmail.com>
2021-03-21 18:48:05 +0300

    remake gallery.tsx begin

--- src/js/components/gallery/gallery.tsx
+++ src/js/components/gallery/gallery.tsx
@@ -1,37 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import SwiperCore, { Navigation, Lazy } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './gallery.module.scss';
-import PaginationChevron from '../pagination-chevron/pagination-chevron';
 import store from '../../../store/store';
-import { changeCurrentPhoto, toggleGalleryImageLoaded } from '../../../store/gallery/actions';
+import { changeCurrentPhoto } from '../../../store/gallery/actions';
 import Footer from './footer/footer';
 import ObjectMap from './object-map/object-map';
 import ModalHeader from '../modal-header/modal-header';
 import { togglePopupVisible } from '../../../store/popup/actions';
 import { IGalleryState } from '../../../store/gallery/types';
 import Image from './image';
+import NavigationPrev from '../section-hero/navigation-prev';
+import NavigationNext from '../section-hero/navigation-next';
+
+SwiperCore.use([Navigation, Lazy]);
 
 interface Props {
   visibleMap: boolean;
-  photoIndex: number;
 }
 // TODO add swipes for slider
 // TODO add animations to gallery
 // TODO add descriptions to photos
 const Gallery = ({ ...props }: Props): JSX.Element => {
   const { project } = store.getState().gallery;
-  const { photoIndex } = props;
   const { visibleMap } = props;
 
-  const handlerClickPrev = () => {
-    store.dispatch(toggleGalleryImageLoaded(false));
-    const newPhotoIndex = photoIndex === 0 ? project.photos.length - 1 : photoIndex - 1;
-    store.dispatch(changeCurrentPhoto(newPhotoIndex));
-  };
-  const handlerClickNext = () => {
-    store.dispatch(toggleGalleryImageLoaded(false));
-    store.dispatch(changeCurrentPhoto(photoIndex === project.photos.length - 1 ? 0 : photoIndex + 1));
-  };
   const handlerClose = () => {
     store.dispatch(changeCurrentPhoto(0));
     store.dispatch(togglePopupVisible(false));
@@ -40,15 +34,25 @@ const Gallery = ({ ...props }: Props): JSX.Element => {
   return (
     <>
       <div className={styles.wrapper}>
-        <div>
-          <Image />
-        </div>
-        <PaginationChevron
-          handlerClickNext={handlerClickNext}
-          handlerClickPrev={handlerClickPrev}
-          textPrev="назад"
-          textNext="вперед"
-        />
+        <Swiper
+          tag="div"
+          wrapperTag="ul"
+          navigation={{
+            prevEl: '.button__prev',
+            nextEl: '.button__next',
+          }}
+          preloadImages={false}
+          lazy={{ loadPrevNext: true }}
+        >
+          {project.photos.map((it) => (
+            <SwiperSlide tag="li" key={it}>
+              <Image imageNumber={it} projectNumber={project.id} />
+            </SwiperSlide>
+          ))}
+          <NavigationPrev />
+          <NavigationNext />
+        </Swiper>
+
         <ModalHeader header={project.name} buttonMapVisible zedIndex={300} handlerClose={handlerClose} />
         <Footer />
         <ObjectMap visible={visibleMap} />
@@ -59,7 +63,6 @@ const Gallery = ({ ...props }: Props): JSX.Element => {
 
 const mapState = (state: IGalleryState) => ({
   visibleMap: state.gallery.mapVisible,
-  photoIndex: state.gallery.photoIndex,
   project: state.gallery.project,
 });
 
--- src/js/components/gallery/image.tsx
+++ src/js/components/gallery/image.tsx
@@ -1,29 +1,24 @@
-import React, { useEffect, useRef } from 'react';
-import store from '../../../store/store';
-import { toggleGalleryImageLoaded } from '../../../store/gallery/actions';
+import React from 'react';
 import styles from './gallery.module.scss';
 import { setTrailingZeros } from '../../utils/functions';
 
-const Image = (): JSX.Element => {
-  const { project } = store.getState().gallery;
-  const { photoIndex } = store.getState().gallery;
-  const projectNameNumber = setTrailingZeros(project.id, 3);
-  const imageNameNumber = setTrailingZeros(project.photos[photoIndex], 3);
-  const image: React.Ref<HTMLImageElement> = useRef(null);
+interface Props {
+  projectNumber: number;
+  imageNumber: number;
+}
+
+const Image = ({ ...props }: Props): JSX.Element => {
+  const { projectNumber } = props;
+  const { imageNumber } = props;
+  const projectNameNumber = setTrailingZeros(projectNumber, 3);
+  const imageNameNumber = setTrailingZeros(imageNumber, 3);
 
   const fileNamePrefix = `/img/photos/gallery/${projectNameNumber}__${imageNameNumber}-photo-`;
-  const handlerImageLoad = () => {
-    store.dispatch(toggleGalleryImageLoaded(true));
-  };
-
-  useEffect(() => {
-    image.current.src = `${fileNamePrefix}1x.jpg, ${fileNamePrefix}2x.jpg 2x`;
-  });
 
   return (
-    <picture style={{ display: 'flex' }}>
+    <picture>
       <source type="image/webp" srcSet={`${fileNamePrefix}1x.webp, ${fileNamePrefix}2x.webp 2x`} />
-      <img src="" className={styles.image} onLoad={handlerImageLoad} alt="Фото ангара" ref={image} />
+      <img src={`${fileNamePrefix}1x.jpg, ${fileNamePrefix}2x.jpg 2x`} className={styles.image} alt="Фото ангара" />
     </picture>
   );
 };
