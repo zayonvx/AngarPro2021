5e4682145a8be2a148bdccf3eee06baf8a8b9683
romez <svetbox@gmail.com>
2021-03-24 22:40:57 +0300

    add navigation point news

--- src/js/components/header/nav-menu-const.ts
+++ src/js/components/header/nav-menu-const.ts
@@ -1,7 +1,8 @@
 const menuList = [
   { id: 1, text: 'портфолио', link: '/#pholio' },
   { id: 2, text: 'о нас', link: '/#choose' },
-  { id: 3, text: 'контакты', link: '/#contacts' },
+  { id: 3, text: 'новости', link: '/#news' },
+  { id: 4, text: 'контакты', link: '/#contacts' },
 ];
 
 const notError = 0;
--- src/js/components/section-news/news.tsx
+++ src/js/components/section-news/news.tsx
@@ -4,7 +4,7 @@ import HeaderOfSection from '../header-of-section/header-of-section';
 import NewsSwiper from './news-swiper';
 
 const News = (): JSX.Element => (
-  <div className={styles.section}>
+  <div className={styles.section} id="news">
     <HeaderOfSection header="Наши новости" text="Отчеты о проделанной работе" />
     <div className={styles.ribbon}>
       <NewsSwiper />
